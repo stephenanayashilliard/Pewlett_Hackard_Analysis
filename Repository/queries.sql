2443d8a52703d32d681b201d17144fcf3a32a4e2
Stephen Anayas Hilliard <stephenanayashilliard@gmail.com>
2020-10-27 06:28:21 -0700

    Retirement.info.csv

--- Repository/queries.sql
+++ Repository/queries.sql
@@ -47,3 +47,9 @@ CREATE TABLE dept_emp(
 	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
 	PRIMARY KEY (emp_no, dept_no)
 );
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
